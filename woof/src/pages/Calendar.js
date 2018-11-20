06d1df3cef0db2c3f2d1f14ab91ad2b62799cf14
Jonathan Gomez <jgomezm@emory.edu>
2018-11-20 02:28:35 -0500

    Tasks limited to 5 hours per day

--- woof/src/pages/Calendar.js
+++ woof/src/pages/Calendar.js
@@ -9,12 +9,13 @@ import {Agenda} from 'react-native-calendars';
 import ActionButton from "react-native-action-button";
 import Icon from "react-native-vector-icons/Ionicons";
 import {Actions} from "react-native-router-flux";
-import PriorityQueue from  "js-priority-queue";
+import PriorityQueue from "js-priority-queue";
 
 export default class AgendaScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            first: true,
             items: {},
             //tasks: new PriorityQueue({ comparator: {function(a, b) { return b.die - a.die }} })
             tasks: new PriorityQueue()
@@ -25,7 +26,7 @@ export default class AgendaScreen extends Component {
         return (
             <Agenda
                 items={this.state.items}
-                tasks = {this.state.tasks}
+                tasks={this.state.tasks}
                 loadItemsForMonth={this.loadItems.bind(this)}
                 selected={this.onDayPress}
                 renderItem={this.renderItem.bind(this)}
@@ -59,44 +60,25 @@ export default class AgendaScreen extends Component {
     loadItems(day) {
         const {items} = this.state
         const {tasks} = this.state
-        fetch('http://durian-django-env.nihngkspzc.us-east-1.elasticbeanstalk.com/event/').then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            const newItems = {}
-            for (var i = 0; i < data.length; i++) {
-                var time = data[i]['startTime']
-                var header = data[i]['header']
-                time = new Date(time)
-                var strTime = time.toISOString().split('T')[0]
-                var endtime = new Date(data[i]['endtime'])
-                var len = ((endtime - time)/60)/1000
-                var endtime = endtime.toISOString().split('T')[1]
-                var year = strTime.slice(0,4)
-                if (!items[strTime]) {
-                    items[strTime] = [];
-                    items[strTime].push({
-                        name: header,
-                        date: strTime.slice(5,10) + "-" + year,
-                        startTime: time.toISOString().split('T')[1].slice(0, -8),
-                        endTime: endtime.slice(0, -8),
-                        desc: data[i]['description'],
-                        loc: data[i]['location'],
-                        height: Math.max(30, Math.floor(len)),
-                        duration: len,
-                        id: data[i]['id']
-                    });
-                }
-                else {
-                    var dupe = false
-                    for (var j = 0; j < items[strTime].length; j++) {
-                        if (items[strTime][j]["id"] == data[i]["id"]) {
-                            dupe = true
-                        }
-                    }
-                    if (!dupe) {
+        if (this.state.first) {
+            fetch('http://durian-django-env.nihngkspzc.us-east-1.elasticbeanstalk.com/event/').then(function (response) {
+                return response.json()
+            }).then(function (data) {
+                const newItems = {}
+                for (var i = 0; i < data.length; i++) {
+                    var time = data[i]['startTime']
+                    var header = data[i]['header']
+                    time = new Date(time)
+                    var strTime = time.toISOString().split('T')[0]
+                    var endtime = new Date(data[i]['endtime'])
+                    var len = ((endtime - time) / 60) / 1000
+                    var endtime = endtime.toISOString().split('T')[1]
+                    var year = strTime.slice(0, 4)
+                    if (!items[strTime]) {
+                        items[strTime] = [];
                         items[strTime].push({
                             name: header,
-                            date: strTime.slice(5,10) + "-" + year,
+                            date: strTime.slice(5, 10) + "-" + year,
                             startTime: time.toISOString().split('T')[1].slice(0, -8),
                             endTime: endtime.slice(0, -8),
                             desc: data[i]['description'],
@@ -106,65 +88,87 @@ export default class AgendaScreen extends Component {
                             id: data[i]['id']
                         });
                     }
-                }
-                for (var k = -15; k < 16; k++) {
-                    var newDay = day.timestamp + k * 24 * 60 * 60 * 1000;
-                    newDay = new Date(newDay)
-                    var strTime = newDay.toISOString().split('T')[0]
-                    if (!items[strTime]) {
-                        items[strTime] = []
+                    else {
+                        var dupe = false
+                        for (var j = 0; j < items[strTime].length; j++) {
+                            if (items[strTime][j]["id"] == data[i]["id"]) {
+                                dupe = true
+                            }
+                        }
+                        if (!dupe) {
+                            items[strTime].push({
+                                name: header,
+                                date: strTime.slice(5, 10) + "-" + year,
+                                startTime: time.toISOString().split('T')[1].slice(0, -8),
+                                endTime: endtime.slice(0, -8),
+                                desc: data[i]['description'],
+                                loc: data[i]['location'],
+                                height: Math.max(30, Math.floor(len)),
+                                duration: len,
+                                id: data[i]['id']
+                            });
+                        }
                     }
-                };
-                for (var k = -15; k < 16; k++) {
-                    var newDay = day.timestamp + k * 24 * 60 * 60 * 1000;
-                    newDay = new Date(newDay)
-                    var strTime = newDay.toISOString().split('T')[0]
-                    if (!items[strTime]) {
-                        items[strTime] = []
+                    for (var k = -15; k < 16; k++) {
+                        var newDay = day.timestamp + k * 24 * 60 * 60 * 1000;
+                        newDay = new Date(newDay)
+                        var strTime = newDay.toISOString().split('T')[0]
+                        if (!items[strTime]) {
+                            items[strTime] = []
+                        }
+                    }
+                    ;
+                    for (var k = -15; k < 16; k++) {
+                        var newDay = day.timestamp + k * 24 * 60 * 60 * 1000;
+                        newDay = new Date(newDay)
+                        var strTime = newDay.toISOString().split('T')[0]
+                        if (!items[strTime]) {
+                            items[strTime] = []
+                        }
                     }
                 }
-            }
-        });
-        fetch('http://durian-django-env.nihngkspzc.us-east-1.elasticbeanstalk.com/task/?format=json').then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            const newItems = {}
-            for (var i = 0; i < data.length; i++) {
-                var header = data[i]['header'].toString()
-                //time = Date.now()
-                //var strTime = time.toISOString().split('T')[0]
-                //Alert.alert(strTime)
-                //if (!tasks) {
-                //tasks[strTime] = [];
-                tasks.queue({
-                    name: header,
-                    desc: data[i]['description'],
-                    priority: data[i]['priority'],
-                    duration: data[i]['duration'],
-                    due: data[i]['due'],
-                    height: Math.max(60, Math.floor(Number(data[i]['duration']))),
-                    id: "Task - " + data[i]['id'].toString(),
-                    die: data[i]['id']  / ((data[i]['priority'] + 1) * data[i]['duration'])
-                });
-            }
-        });
-        this.makeEvents()
+            });
+            fetch('http://durian-django-env.nihngkspzc.us-east-1.elasticbeanstalk.com/task/?format=json').then(function (response) {
+                return response.json()
+            }).then(function (data) {
+                const newItems = {}
+                for (var i = 0; i < data.length; i++) {
+                    var header = data[i]['header'].toString()
+                    //time = Date.now()
+                    //var strTime = time.toISOString().split('T')[0]
+                    //Alert.alert(strTime)
+                    //if (!tasks) {
+                    //tasks[strTime] = [];
+                    tasks.queue({
+                        name: header,
+                        desc: data[i]['description'],
+                        priority: data[i]['priority'],
+                        duration: data[i]['duration'],
+                        due: data[i]['due'],
+                        height: Math.max(60, Math.floor(Number(data[i]['duration']))),
+                        id: "Task - " + data[i]['id'].toString(),
+                        die: data[i]['id'] / ((data[i]['priority'] + 1) * data[i]['duration'])
+                    });
+                }
+            });
+            this.makeEvents()
+        }
     }
 
     renderItem(item) {
         const id = item.id.toString()
-        const letter = id.slice(0,1)
+        const letter = id.slice(0, 1)
         const b = Boolean(letter == "T")
-        if (b){
-            return(
+        if (b) {
+            return (
                 <TouchableHighlight onPress={() => this.seeEvent(item)}>
                     <View style={[styles.task, {height: item.height}]}><Text>{item.name}</Text>
                     </View>
                 </TouchableHighlight>
             );
         }
-        else{
-            return(
+        else {
+            return (
                 <TouchableHighlight onPress={() => this.seeEvent(item)}>
                     <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text>
                     </View>
@@ -173,27 +177,47 @@ export default class AgendaScreen extends Component {
         }
     }
 
-
     makeEvents(){
         const {tasks} = this.state
         const {items} = this.state
-        time = new Date()
+        var time = new Date()
         var strTime = time.toISOString().split('T')[0]
-        var year = strTime.slice(0,4)
+        var year = strTime.slice(0, 4)
         while (tasks.length > 0){
-            var t = tasks.dequeue()
-            items[strTime].push({
-                name: t.name,
-                date: strTime.slice(5,10) + "-" + year,
-                startTime: "",
-                endTime: "",
-                desc: t.description,
-                loc: "",
-                height: t.height,
-                id: t.id
-            });
+            if (!items[strTime]) {
+                    items[strTime] = []
+                }
+                else {
+                    const today = items[strTime]
+                    var total = 0
+                    for (var i = 0; i < today.length; i++) {
+                        total = total + today[i].duration
+                    }
+                }
+                total = total + tasks.peek().duration
+                if (total < 300) {
+                    var t = tasks.dequeue()
+                    items[strTime].push({
+                        name: t.name,
+                        date: strTime.slice(5, 10) + "-" + year,
+                        startTime: "",
+                        endTime: "",
+                        desc: t.description,
+                        loc: "",
+                        height: t.height,
+                        duration: t.duration,
+                        id: t.id
+                    });
+                }
+                else{
+                    total = 0
+                    time.setTime(time.getTime() + 24 * 60 * 60 * 1000)
+                    time = new Date(time)
+                    strTime = time.toISOString().split('T')[0]
+                }
         }
     }
+    
 
     renderEmptyDate(day) {
         return (
