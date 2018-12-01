0817b4c21d2e121e013b675bd828d6ec6d29ba1b
alvinjchoi <31370892+alvinjchoi@users.noreply.github.com>
2018-12-01 14:20:03 -0500

    font

--- woof/src/pages/Login.js
+++ woof/src/pages/Login.js
@@ -181,11 +181,10 @@ const styles = StyleSheet.create({
         height: 120,
     },
     logoText: {
-        fontFamily: 'PT Sans Bold',
+        fontFamily: "PT-Sans",
         color: 'white',
         fontSize: 40,
         justifyContent: 'center',
-        fontWeight: 'bold',
         marginTop: 10,
         // opacity: ,
     },
