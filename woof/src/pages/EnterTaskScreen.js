94d00f7a0bbf4774895f539864239a6abd90f0cc
Nandar Soe <snandar.nsoe@gmail.com>
2018-12-01 22:49:24 -0500

    Update EnterTaskScreen.js

--- woof/src/pages/EnterTaskScreen.js
+++ woof/src/pages/EnterTaskScreen.js
@@ -41,7 +41,6 @@ export default class EnterTaskScreen extends React.Component {
       taskdescription: '',
       priority: 1,
       chosenpri: 1,
-      due: null,
       duration: '00:30',
       chosendur: '30',
       chosendue: null,
@@ -60,6 +59,19 @@ export default class EnterTaskScreen extends React.Component {
       if(this.state.taskname == ''){
           Alert.alert("Please enter Task Name.");
       } else{
+          if(this.state.checkdur){
+              var len = this.state.duration.length;
+              this.state.chosendur = parseInt(this.state.duration.substring(len-2, len), 10);
+              this.state.chosendur += parseInt(this.state.duration.substring(0, len-3), 10) * 60;
+          } 
+          if(this.state.checkpri) this.state.chosenpri =  this.state.priority;
+          if(this.state.checkdate) {
+              if(this.state.checktime){
+                  this.state.chosendue = this.state.dated + ' ' + this.state.timed;
+              } else{
+                  this.state.chosendue = this.state.dated + ' 00:00';
+              }
+          }
           this.tasktest()
           this.props.navigation.navigate('todolist')
       }
@@ -79,6 +91,20 @@ export default class EnterTaskScreen extends React.Component {
       alert("button did a change to " + !this.state.checkpri)
   }
 
+  changedate(){
+    this.setState({
+        checkdate: !this.state.checkdate
+    })
+      alert("button did a change to " + !this.state.checkdate)
+  }
+
+  changetime(){
+    this.setState({
+        checktime: !this.state.checktime
+    })
+      alert("button did a change to " + !this.state.checktime)
+  }
+
 tasktest = () =>{
     fetch('http://durian-django-env.nihngkspzc.us-east-1.elasticbeanstalk.com/task/', {
     method: 'POST',
@@ -88,16 +114,15 @@ tasktest = () =>{
     body: JSON.stringify({
       header: this.state.taskname,
       description: this.state.taskdescription,
-      priority: this.state.priority,
-      duration: (parseInt(this.state.duration.substring(3, 5), 10) + (parseInt(this.state.duration.substring(0, 2), 10) * 60)),
-      due: this.state.due,
+      priority: this.state.chosenpri,
+      duration: this.state.chosendur,
+      due: this.state.chosendue,
       owner: 'http://durian-django-env.nihngkspzc.us-east-1.elasticbeanstalk.com/profile/1/',
     }),
 })
   .then((response) => response.json())
   .then((responseJson) => {
-//Alert.alert("Task Successfully Added.");
-Alert.alert((parseInt(this.state.duration.substring(3, 5), 10) + (parseInt(this.state.duration.substring(0, 2), 10) * 60)) + " " + this.state.priority);
+Alert.alert("Task Successfully Added.");
   })
   .catch((error) => {
     console.error(error);
@@ -132,74 +157,126 @@ Alert.alert((parseInt(this.state.duration.substring(3, 5), 10) + (parseInt(this.
         </View>
 
         <View style = {{flexDirection: 'row', paddingLeft: 40}}>
+          <CheckBox
+            onChange={this.changedur.bind(this)}
+            title="checkdur"
+            checked={this.state.checkdur}
+          />
 
-          <Text style={{paddingTop: 10}}>Duration: </Text>
+          <Text style={{paddingTop: 10, fontFamily: 'Montserrat-ExtraLight', color: '#fff'}}>Duration: </Text>
 
           <DatePicker
-          style={{width: 200}}
-          date={this.state.duration}
-          mode="time"
-          placeholder="select date"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          showIcon={false}
-          customStyles={{
+            style={{width: 211, alignItems: 'center'}}
+            date={this.state.duration}
+            mode="time"
+            placeholder="select date"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            iconSource={require('../images/stopwatch.png')}
+            customStyles={{
+              dateInput: {
+                marginLeft: 12,
+                alignItems: 'center'
+              },
               dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-            // ... You can check the source to find the other keys. {(durationFormat) => {this.changedur({date: durationFormat})}}
-          }}
-          onDateChange={(duration) => this.setState({duration})}/>
+                //position: 'absolute',
+                left: 10,
+                top: 4,
+                marginLeft: 0
+              }
+              // ... You can check the source to find the other keys.
+            }}
+            onDateChange={(date) => {this.setState({duration: date})}}
+          />
         </View>
         
         <View style = {{flexDirection: 'row', paddingLeft: 40}}>
+          
+          <CheckBox
+            onChange={this.changepri.bind(this)}
+            title="checkpri"
+            checked={this.state.checkpri}
+          />
 
-          <Text style={{paddingTop: 10}}>Priority:</Text>
+          <Text style={{paddingTop: 10, fontFamily: 'Montserrat-ExtraLight', color: '#fff', paddingRight: 25}}>Priority:</Text>
 
-          <Image source={require('../images/nimportant.png')} style={{width: 30, height: 30}} />
+          {/* <Image source={require('../images/nimportant.png')} style={{width: 30, height: 30}} /> */}
           <Slider
-            style={{width: 200}}
+            style={{width: 200, paddingLeft: 100,}}
             minimumValue={1}
             maximumValue={5}
             step={1}
             onValueChange={(priority) => this.setState({ priority })}
             value={this.state.priority}
           />
-          <Image source={require('../images/vimportant.png')} style={{width: 30, height: 30}} />
+          {/* <Image source={require('../images/vimportant.png')} style={{width: 30, height: 30}} /> */}
         </View>
 
         <View style = {{flexDirection: 'row', paddingLeft: 40}}>
-          <Text style={{paddingTop: 10}}>Due Date:</Text>
+          <CheckBox
+            onChange={this.changedate.bind(this)}
+            title="checkdate"
+            checked={this.state.checkdate}
+          />
+          <Text style={{paddingTop: 10, fontFamily: 'Montserrat-ExtraLight', color: '#fff'}}>Date: {"\n"}</Text>
           <DatePicker
             style={{width: 250}}
-            date={this.state.due}
-            mode="datetime"
-            placeholder="select date (optional)"
-            format="YYYY-MM-DD HH:mm"
+            date={this.state.dated}
+            mode="date"
+            placeholder="select date"
+            format="YYYY-MM-DD"
+            minDate="2016-05-01"
+            maxDate="2029-12-31"
             confirmBtnText="Confirm"
             cancelBtnText="Cancel"
-            showIcon={false}
             customStyles={{
               dateIcon: {
-                position: 'absolute',
+                // position: 'absolute',
                 left: 0,
                 top: 4,
-                marginLeft: 0
-                },
-                dateInput: {
-                  marginLeft: 36
-                }
-                // ... You can check the source to find the other keys. {(durationFormat) => {this.changedur({date: durationFormat})}}
+                marginLeft: 10
+              },
+              dateInput: {
+                marginLeft: 40
+              }
+              // ... You can check the source to find the other keys.
             }}
-            onDateChange={(due) => this.setState({due})}/>
+            onDateChange={(date) => {this.setState({dated: date})}}
+          />
         </View>
-      
+        
+
+        <View style = {{flexDirection: 'row', paddingLeft: 40}}>
+          <CheckBox
+            onChange={this.changetime.bind(this)}
+            title="checktime"
+            checked={this.state.checktime}
+          />
+          <Text style={{paddingTop: 10, fontFamily: 'Montserrat-ExtraLight', color: '#fff'}}>Time:</Text>
+          <DatePicker
+            style={{width: 253}}
+            date={this.state.timed}
+            mode="time"
+            placeholder="select date"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            iconSource={require('../images/clock.png')}
+            customStyles={{
+              dateIcon: {
+                //position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 11
+              },
+              dateInput: {
+                marginLeft: 42
+              }
+              // ... You can check the source to find the other keys.
+            }}
+            onDateChange={(date) => {this.setState({timed: date})}}
+          />
+          
+        </View>
         
         <View style={styles.addButtonContainer}>
           <Button
@@ -220,25 +297,32 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 36,
     marginTop: 60,
-    fontWeight: '300'
+    fontWeight: '300',
+    fontFamily: 'Montserrat-ExtraLight',
   },
   inputContainer: {
     marginTop: 10,
     alignItems: 'center',
+    fontFamily: 'Montserrat-ExtraLight',
   },
   input: {
-    width: WIDTH - 90, //left-right text input size
-    height: 40,         //up-down text input size
-    borderRadius: 20,
+    width: WIDTH - 55,
+    height: 45,
+    borderColor: 'rgba(0,0,0,1)',
     fontSize: 16,
-    paddingLeft: 40,  //inside text paddinging
-    backgroundColor: 'rgba(0,0,0,0.35)',
-    color: '#EADCD9',
-    marginHorizontal: 40,
+    paddingLeft: 45,
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    color: 'rgba(0, 0, 0, 1)',
+    marginHorizontal: 25,
+    borderRadius: 45,
+    marginTop: 10,
+    marginBottom: 10,
+    fontFamily: 'Montserrat-ExtraLight',
   },
   dateContainer: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    fontFamily: 'Montserrat-ExtraLight',
   },
   addButtonContainer: {
     paddingTop: 50
@@ -253,6 +337,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 19,
     textAlign: 'center',
+    fontFamily: 'Montserrat-ExtraLight',
   },
   contentContainer: {
     paddingTop: 0,
@@ -278,11 +363,13 @@ const styles = StyleSheet.create({
   },
   codeHighlightText: {
     color: 'rgba(96,100,109, 0.8)',
+    fontFamily: 'Montserrat-ExtraLight',
   },
   codeHighlightContainer: {
     backgroundColor: 'rgba(0,0,0,0.05)',
     borderRadius: 3,
     paddingHorizontal: 4,
+    fontFamily: 'Montserrat-ExtraLight',
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -308,6 +395,7 @@ const styles = StyleSheet.create({
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     textAlign: 'center',
+    fontFamily: 'Montserrat-ExtraLight',
   },
   navigationFilename: {
     marginTop: 5,
@@ -322,6 +410,7 @@ const styles = StyleSheet.create({
   helpLinkText: {
     fontSize: 14,
     color: '#2e78b7',
+    fontFamily: 'Montserrat-ExtraLight',
   },
 });
 
